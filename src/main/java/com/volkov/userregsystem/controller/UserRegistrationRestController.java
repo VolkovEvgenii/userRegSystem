6ac67217f97d868af3b63a64910c92f71a3b6124
Evgenii <volkov.e.n@gmail.com>
2020-08-28 15:21:39 +0300

    listAllUser add to UserRegistrationRetController

--- src/main/java/com/volkov/userregsystem/controller/UserRegistrationRestController.java
+++ src/main/java/com/volkov/userregsystem/controller/UserRegistrationRestController.java
@@ -1,11 +1,16 @@
 package com.volkov.userregsystem.controller;
 
 
+import com.volkov.userregsystem.dto.UserDTO;
 import com.volkov.userregsystem.repository.UserJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -17,5 +22,10 @@ public class UserRegistrationRestController {
 
     public void setUserJpaRepository(UserJpaRepository userJpaRepository) {
         this.userJpaRepository = userJpaRepository;
+    }
+
+    public ResponseEntity<List<UserDTO>> listAllUsers() {
+        List<UserDTO> users = userJpaRepository.findAll();
+        return new ResponseEntity<List<UserDTO>>(users, HttpStatus.OK);
     }
 }
